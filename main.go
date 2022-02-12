7bede8e6257519fc4f5ea8efca9c2cb49cef2e12
Deepak Vashist <deepaksvashist@gmail.com>
2022-02-12 18:03:38 -0300

    feat: create graceful shutdown implementation

--- main.go
+++ main.go
@@ -1,3 +1,43 @@
 package main
 
-func main() {}
+import (
+	"context"
+	"errors"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func main() {
+	server := http.Server{
+		Addr: ":8000",
+	}
+
+	http.Handle("/", http.FileServer(http.Dir("./public")))
+
+	go func() {
+		log.Println("starting http server")
+
+		if err := server.ListenAndServe(); !errors.Is(err, http.ErrServerClosed) {
+			log.Fatalf("http server error %v", err)
+		}
+
+		log.Println("stopped serving new connections")
+	}()
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+	<-sigChan
+
+	shutdownCtx, shutdownRelease := context.WithTimeout(context.Background(), 10*time.Second)
+	defer shutdownRelease()
+
+	if err := server.Shutdown(shutdownCtx); err != nil {
+		log.Fatalf("http shutdown error %v", err)
+	}
+
+	log.Println("graceful shutdown complete")
+}
